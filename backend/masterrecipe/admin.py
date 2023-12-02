da0000f39cab6513b2c119312a05363d6d0c4f09
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-02 13:11:48 +0300

    Add new models and serializers #7

--- backend/masterrecipe/admin.py
+++ backend/masterrecipe/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from .models import Direction, Recipe, User, Ingredient
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(User)
+admin.site.register(Ingredient)
+admin.site.register(Direction)
--- backend/masterrecipe/models.py
+++ backend/masterrecipe/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
@@ -8,3 +9,30 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='recipes', blank=True, null=True,)
+    description = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=125)
+    amount = models.CharField(max_length=250, blank=True, null=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients", blank=True, null=True)
+    ## ADD Quantity and Standard measurements later ##
+
+    def __str__(self):
+        return f"{self.name} in {self.recipe}"
+
+class Direction(models.Model):
+    content = models.CharField(max_length=250)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="directions", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} in {self.recipe}"
