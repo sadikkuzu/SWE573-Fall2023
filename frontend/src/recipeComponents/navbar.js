899bf41dd8d9edfcbe4ca300787c6f4843eba208
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-17 14:42:15 +0300

    Add navbar component #7

---
+++ frontend/src/recipeComponents/navbar.js
@@ -0,0 +1,91 @@
+import React, { useContext, useState } from 'react';
+
+
+// ReactStrap
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+// Routing
+import {Outlet, Link} from "react-router-dom";
+
+
+//Auth
+import AuthContext from '../context/AuthContext';
+
+//Styling
+import './navbar.css'
+
+
+
+export default function NavBar(props){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    const {user, logoutUser} = useContext(AuthContext);
+
+    return (
+        <div>
+            <Navbar dark expand='md' id='navbar'>
+                <NavbarBrand href="/"> MasterRecipe </NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className='me-auto' navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/RecipeCreate">
+                                Create Recipe
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/RecipeList">
+                                Recipe List
+                            </NavLink>
+                        </NavItem>
+                        
+                        
+                        {user ? (
+                            <>
+                                <NavItem>
+                                    <NavLink tag={Link} onClick={logoutUser}>
+                                        Logout
+                                    </NavLink>
+                                </NavItem>
+                            </> 
+                            ) : (
+                                <>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/Login">
+                                        Login
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} to="/Register">
+                                        Register
+                                    </NavLink>
+                                </NavItem>
+                                </>
+                            )
+                        }
+                        
+                    </Nav>
+                </Collapse>
+            </Navbar>
+
+            <div id="detail">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
