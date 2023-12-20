96557aa8e44d809fea8d442d15ffeae56cd2c992
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-20 23:30:14 +0300

    Add recipe components #7

---
+++ frontend/src/recipeComponents/recipeList.js
@@ -0,0 +1,190 @@
+import React, { useCallback, useEffect, useState, useContext } from "react";
+
+// Routing and Request
+import axios from "axios";
+import { useLoaderData, useNavigate } from 'react-router-dom'
+
+// Styling
+import { Card, CardBody, CardTitle, CardText, CardSubtitle, Button, Input, Label, FormGroup } from 'reactstrap'
+import searchIcon from './images/searchIcon.png'
+import './recipeList.css'
+
+// Components
+import NavBar from './navbar';
+
+// Auth
+import AuthContext from "../context/AuthContext";
+
+
+
+const { DateTime } = require('luxon');
+
+
+export const listLoader = async () => {
+    console.log("starting list loader");
+
+    const backendHost = process.env.REACT_APP_MASTERRECIPE_BACKEND_HOST || 'http://localhost';
+    const backendPort = process.env.REACT_APP_MASTERRECIPE_BACKEND_PORT || 8000;
+    const backendEndpoint = process.env.REACT_APP_MASTERRECIPE_BACKEND_ENDPOINT || 'api';
+
+    const results = await axios.get(`${backendHost}:${backendPort}/${backendEndpoint}/`)
+    .catch((error) =>{
+        console.log('Error', error.message);
+    }); 
+    const recipes = results.data;
+    return recipes;
+}
+
+
+function SearchBarContainer (props) {
+
+    return (
+        <>
+        
+        <form>
+            <div className="search-wrapper mb-3 mt-3">
+                <div className="search">
+                    <input className="searchTerm" placeholder="Search" type="text" onChange={(e) => props.onChange(e)}/>
+                    <Button type="submit" className="searchButton">
+                        <img className="searchIcon" src={searchIcon}  alt="Icon"/>
+                    </Button>
+                </div>
+            </div>
+        </form>
+        {
+         props.user && 
+            <div className="checkbox-wrapper d-flex justify-content-center">
+            <FormGroup switch>
+                <Input 
+                    type="switch" 
+                    role="switch"
+                    checked={props.isChecked}
+                    onChange={()=>{props.setIsChecked(!props.isChecked)}}
+                />
+                <Label check>View only My Recipes</Label>
+            </FormGroup>
+            </div>  
+        }
+        
+        </>
+    )
+}
+
+function RecipeCard(props){
+
+    const created_at = DateTime.fromISO(props.recipe.created_at).toLocaleString(DateTime.DATETIME_MED);
+    const navigate = useNavigate();
+    const handleOnClick = useCallback(() => navigate(`/Recipe/${props.recipe.id}`, {replace:true}),[navigate]);
+
+    return(
+        <>
+        <Card
+        style={{
+        width: '18rem'
+        }}
+        onClick={handleOnClick}
+        className='recipe-card m-3'
+        color="light"
+        >
+            <CardBody>
+                <CardTitle className="card-title" tag="h4">
+                    {props.recipe.name}
+                </CardTitle>
+                <CardSubtitle className="mb-2" tag="h6">
+                    &nbsp; {/* By: {props.recipe.owner} */}
+                    <br />
+                    &nbsp; {/* {created_at}  */}
+                </CardSubtitle>
+            </CardBody>
+            <img className="card-image" alt="Card cap" src={props.recipe.image} width="100%"/>
+            <CardBody >
+                <CardText>
+                    {props.recipe.description ? props.recipe.description : " A quick and simple Recipe!"}
+                </CardText>
+            </CardBody>
+        </Card>
+        
+        </>
+    )
+}
+
+
+function CardsContainer (props){
+
+    function CreateCards(){
+        let searchValue = props.searchValue.toLowerCase()
+
+        if (props.searchValue === "") {
+            return(props.recipes.map((recipe) => {
+                return <RecipeCard name={recipe.name} recipe={recipe} key={recipe.id}/>
+            }))
+        } else {
+            return(props.recipes.map((recipe) => {
+                let recipeName = recipe['name'].toLowerCase()
+                if (recipeName.includes(searchValue)){
+                    return <RecipeCard name={recipe.name} recipe={recipe} key={recipe.id}/>
+                }
+                return null
+            }))
+        }
+        
+    }
+
+
+    return (
+        <>
+        <div className="cards-container d-flex flex-wrap justify-content-center mb-3">
+            <CreateCards />
+        </div>
+        </>
+    )
+}
+
+
+export default function RecipeListContainer () {
+    const [searchValue, setSearchValue] = useState("")
+    const [recipes, setRecipes] = useState([])
+    const [isChecked, setIsChecked] = useState(false)
+
+    const {user} = useContext(AuthContext);
+
+    function handleSearchValueChange (e) {
+        setSearchValue(e.target.value);
+    };
+
+    const recipeList = useLoaderData();
+
+    useEffect(() => {
+        setRecipes(recipeList);
+    }, []);
+    
+    useEffect(() => {
+        if (isChecked){
+            const backendHost = process.env.REACT_APP_MASTERRECIPE_BACKEND_HOST || 'http://localhost';
+            const backendPort = process.env.REACT_APP_MASTERRECIPE_BACKEND_PORT || 8000;
+            const backendEndpoint = process.env.REACT_APP_MASTERRECIPE_BACKEND_ENDPOINT || 'api';
+
+            axios.get(`${backendHost}:${backendPort}/${backendEndpoint}/userList/${user.username}`)
+            .then((results) => {
+                console.log(results.data);
+                const userList = results.data
+                setRecipes(userList)
+            })
+            .catch((error) => {
+            console.log('Error', error.message);
+            });
+        } else {
+            setRecipes(recipeList);
+        }
+    }, [isChecked])
+
+    return (
+        <div className="list-background">
+            <NavBar />
+            <div className="container">
+                <SearchBarContainer onChange={handleSearchValueChange} isChecked={isChecked} setIsChecked={setIsChecked} user={user} />
+                <CardsContainer searchValue={searchValue} recipes={recipes}/>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/utils/PrivateRoute.js
@@ -0,0 +1,9 @@
+import { Navigate} from "react-router-dom";
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+    let { user } = useContext(AuthContext)
+    return !user ? <Navigate to="/login" /> : children;
+};
+export default PrivateRoute;
