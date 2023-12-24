7818f7b3d05d3c2ebb0a79edc5bdc51be9f83621
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-24 03:54:13 +0300

    Use StrictMode #7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Bootstrap & styling
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Routing
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  Outlet,
+  Navigate
+} from "react-router-dom";
+
+
+//Auth
+import { AuthProvider } from './context/AuthContext'
+
+// components
+import RecipeCreateContainer from './recipeComponents/recipeCreate';
+import RecipeContainer, {recipeLoader} from './recipeComponents/recipeContainer';
+import RecipeListContainer, {listLoader} from './recipeComponents/recipeList';
+import HomePage, { homeLoader } from './recipeComponents/home';
+import LoginPage from './recipeComponents/loginPage';
+import RegisterPage from './recipeComponents/registerPage';
+import PrivateRoute from './utils/PrivateRoute';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const AuthProviderLayout = () => (
+    <AuthProvider>
+        <Outlet />
+    </AuthProvider>
+)
+
+const JSXRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<AuthProviderLayout />}>
+            <Route path="/" element={<HomePage />} loader={homeLoader} />
+            <Route path="/Recipe/:id" element={<RecipeContainer />} loader={recipeLoader} />
+            <Route 
+                path="/RecipeCreate" 
+                element={
+                    <PrivateRoute>
+                        <RecipeCreateContainer />
+                    </PrivateRoute> 
+                    } 
+            />
+            <Route path="/RecipeList" element={<RecipeListContainer />} loader={listLoader} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            
+        </Route>
+    ))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={JSXRouter}/>
+    </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
