45fd7f439c02764dba90e8decdd416425534612a
SADIK KUZU <sadikkuzu@hotmail.com>
2023-12-26 18:49:44 +0300

    Runserver #11

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver "0.0.0.0:${MASTERRECIPE_BACKEND_PORT:=8000}"
+# python manage.py makemigrations
+# python manage.py migrate
+python manage.py runserver
